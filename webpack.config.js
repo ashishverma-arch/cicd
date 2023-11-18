f92cb409226dc0733b1469efcf386f1ad6cba219
Ashish Verma <ashishverma.ipec@gmail.com>
2023-11-18 19:22:28 +0530

    Modify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
+const path = require('path');
+
 module.exports = {
-    resolve: {
-      fallback: {
-        "http": require.resolve("stream-http"),
-        "buffer": false,
-        "url": require.resolve("url/")
-      }
-    },
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
-        }
-      ]
+  resolve: {
+    fallback: {
+      "http": require.resolve("stream-http"),
+      "buffer": false,
+      "url": require.resolve("url/")
     }
-  };
-  
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
