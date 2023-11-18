b9a033a1d3570afbe1dadd16100942524e0f11c5
Ashish Verma <ashishverma.ipec@gmail.com>
2023-11-18 12:14:12 +0530

    Enhance Code

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var http = require('http');
 
+var server = http.createServer(function(req, resp) {
 
+    resp.writeHead(200, {"Content-Type": "text/plain"});
+    resp.end("Hello World!");
+
+});
+
+var port = 80;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
